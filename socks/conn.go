f2eef113e1a9108c03a8f973884bfed7d801452f
jholdstock <jholdstock@decred.org>
2026-07-20 11:05:30 +0800

    Don't return unspecified bound address.
    Confirm bound address reported by the proxy is not an unspecified
    address to avoid returning an unhelpful value like "0.0.0.0:0" to the
    caller. For example, Tor will return an unspecified address rather than
    disclose the actual address it bound to. Fallback to the address of the
    connection to the proxy itself.

--- socks/conn.go
+++ socks/conn.go
@@ -1,4 +1,5 @@
 // Copyright 2012 Samuel Stauffer. All rights reserved.
+// Copyright (c) 2019-2026 The Decred developers
 // Use of this source code is governed by a 3-clause BSD
 // license that can be found in the LICENSE file.
 
@@ -39,9 +40,18 @@ func (c *proxiedConn) Close() error {
 }
 
 func (c *proxiedConn) LocalAddr() net.Addr {
+	// Confirm bound address reported by the proxy is not an unspecified address
+	// to avoid returning an unhelpful value like "0.0.0.0:0" to the caller. For
+	// example, Tor will return an unspecified address rather than disclose the
+	// actual address it bound to.
 	if c.boundAddr != nil {
-		return c.boundAddr
+		ip := net.ParseIP(c.boundAddr.Host)
+		if ip == nil || !ip.IsUnspecified() {
+			return c.boundAddr
+		}
 	}
+
+	// Fallback to the address of the connection to the proxy itself.
 	return c.conn.LocalAddr()
 }
 
